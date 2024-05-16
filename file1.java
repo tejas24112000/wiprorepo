0d03d678dafc2a2709b2fb35e24140d129b84f45
tejas24112000 <tejasmandwe@gmail.com>
2024-05-16 17:08:16 +0530

    add method add()

--- file1.java
+++ file1.java
@@ -2,6 +2,11 @@ import java.lang.*;
 
 public class File1
 {
+	int x=100;
+	int y=200;
+	public void add(){
+		System.out.println(x+y);
+	}
 	public static void main(String[] args){
 	 System.out.println("First Java");
 	}
